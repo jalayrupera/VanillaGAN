064d67088248226e5b9c9a21496d904bbf334d27
Jalay Rupera <jalayrupera@pop-os.localdomain>
2020-03-16 09:19:10 +0530

    added dependencies

---
+++ main.py
@@ -0,0 +1,6 @@
+#Importing the dependencies
+import torch
+from torch import nn, optim
+from torch.autograd.variable import Variable
+from torchvision import transforms, datasets
+from utils import Logger
