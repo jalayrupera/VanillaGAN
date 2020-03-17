030c04cf2fb4701a495133bef207712addd0578e
Jalay Rupera <jalayrupera@pop-os.localdomain>
2020-03-17 13:02:13 +0530

    error fixed for dataset

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from utils import Logger
 from generator import Generator
 from discriminator import Discriminator
 
+DATA_FOLDER = './torch_data/VGAN/MNIST'
+
 def mnist_data():
-    compose = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((.5, .5, .5), (.5, .5, .5))
-        ])
-    out_dir = './dataset'
+    compose = transforms.Compose([
+        transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+    out_dir = '{}/dataset'.format(DATA_FOLDER)
     return datasets.MNIST(root=out_dir, train=True, transform=compose, download=True)
 # Load data
 data = mnist_data()
